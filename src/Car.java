9f19527236bf7efeba8e0390c7d2cf5f8015fd00
roman <roman.pshenichnikov555@gmail.com>
2022-10-19 22:28:08 +0300

    Course2_Hw1.1done 1/3HW

---
+++ src/Car.java
@@ -0,0 +1,22 @@
+public class Car {
+    String brand;
+    String model;
+    double engineVolume;
+    String color;
+    int year;
+    String country;
+
+    public Car(String brand,String model,int year,String country,String color,double engineVolume) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return brand + " " + model + ", " + year + " года выпуска, сборка в " + country + ", " + color + " цвета, объем двигателя - " + engineVolume;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,12 @@ public class Main {
     public static void separation() {
         System.out.println("========================================================================================================================================");
     }
+
     public static void main(String[] args) {
-        Human maxim = new Human("Максим", "Минск", 2022 - 35,"бренд-менеджера");
-        Human anya = new Human("Аня", "Москва", 2022 - 29,"методистома образовательных программ");
-        Human kate = new Human("Катя", "Калининград", 2022 - 28,"продакт-менеджера");
-        Human artem = new Human("Артем", "Москва", 2022 - 27,"директором по развитию бизнеса");
+        Human maxim = new Human("Максим", "Минск", 2022 - 35, "бренд-менеджера");
+        Human anya = new Human("Аня", "Москва", 2022 - 29, "методистома образовательных программ");
+        Human kate = new Human("Катя", "Калининград", 2022 - 28, "продакт-менеджера");
+        Human artem = new Human("Артем", "Москва", 2022 - 27, "директором по развитию бизнеса");
         separation();
         System.out.println(maxim);
         separation();
@@ -15,6 +16,28 @@ public class Main {
         System.out.println(kate);
         separation();
         System.out.println(artem);
+        separation();
+        // Не понял как в один гит репозиторий 2 проекта сохранить.
+        System.out.println("Задача 3");
+        task3();
+    }
+
+    public static void task3() {
+        Car lada = new Car("Lada", "Granta", 2015, "России", "желтого",1.7);
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германии", "черный", 3.0);
+        Car bmw = new Car("BMW", "Z8", 2021, "Гремании", "черный", 3.0);
+        Car kia = new Car("Kia", "Sportage 4-го поколения", 2018, "Южной Корее", "красный", 2.4);
+        Car hyundai = new Car("Hyundai", "Avante", 2016, "Южной Корее", "оранжевый", 1.6);
+        separation();
+        System.out.println(lada);
+        separation();
+        System.out.println(audi);
+        separation();
+        System.out.println(bmw);
+        separation();
+        System.out.println(kia);
+        separation();
+        System.out.println(hyundai);
         separation();
 
     }
